f93726ca8638189327c8250643833a6471092b7d
cubsfan46350 <cubsfan.46350@msn.com>
2019-05-30 17:47:22 -0500

    added the routes for users

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const db = require('../api/queries');
 
 // test
 router.get("/", (req, res) => {
   res.status(200).json("Quizzer: Auth user API is running");
 });
+
+router.get('/users', db.getUsers)
+router.get('/users/:id', db.getUserById)
+router.post('/users', db.createUser)
+router.put('/users/:id', db.updateUser)
+router.delete('/users/:id', db.deleteUser)
 
 module.exports = router;
