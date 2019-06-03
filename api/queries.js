65a3dfcbc90930c406597eb223a9340caa739bf5
cubsfan46350 <cubsfan.46350@msn.com>
2019-06-03 15:46:04 -0500

    integrated postgres

--- api/queries.js
+++ api/queries.js
@@ -1,72 +1,60 @@
-const pg = require('pg');
-
-
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Quizzer',
-    password: '',
-    port: 5432
-}
-
-const pool = new pg.Pool(config)
-
-
-
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig.development);
 
 //GET USERS
 const getUsers = (req, res) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+     db.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
         }
         
-    return res.status(200).json(results.rows)
+    return res.status(200).json(results.rows);
     });
 }
 //GET USER BY ID
 const getUserById = (req, res) => {
     const id = parseInt(req.params.id)
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results => {
+    db.query('SELECT * FROM users WHERE id = $1', [id], (error, results => {
         if (error) {
             throw error
         }
-        res.status(200).json(results.rows)
+       return res.status(200).json(results.rows);
     }))
 }
 //CREATE USER
 const createUser = (req, res) => {
     const {name, email } = req.body
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    db.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
         if (error) {
             throw error
         }
-        res.status(201).send(`User added with ID: ${result.insertId}`)
+       return res.status(201).send(`User added with ID: ${result.insertId}`);
     })
 }
 //UPDATE USER
 const updateUser = (req, res) => {
     const id = parseInt(req.params.id)
     const { name, email } = req.body
-    pool.query(
+    db.query(
         'UPDATE users SET name = $1, email = $2 WHERE id = $3',
         [name, email, id],
         (error, results) => {
             if (error) {
                 throw error
             }
-            res.status(200).send(`User modified with ID: ${id}`)
+           return res.status(200).send(`User modified with ID: ${id}`);
         }
     )
 }
 //DELETE USER
 const deleteUser = (req, res) => {
     const id = parseInt(req.params.id)
-    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    db.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
         if (error) {
             throw error
         }
-        res.status(200).send(`User deleted with ID: ${id}`)
+        return res.status(200).send(`User deleted with ID: ${id}`);
     })
 }
 
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,32 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
+      database: 'quizzer',
+      user: 'me',
       password: 'password'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
   },
+
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
   }
-
-};
+}
