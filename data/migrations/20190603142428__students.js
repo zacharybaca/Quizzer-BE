424f6d8a535ab3a6b37fc1936c26b44218093039
Marc Thiongo <marcthuo@gmail.com>
2019-06-03 14:53:37 -0400

    update profile route, added model, migration, seed

---
+++ data/migrations/20190603142428__students.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema
+      .createTable('students', tbl => {
+          tbl.increments()
+          tbl.string('name').notNullable();
+          tbl.string('username').notNullable();
+          tbl.string('email').notNullable();
+          tbl.string('role').notNullable();
+      });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('students');
+  };
