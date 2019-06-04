6d12cdf176edd167f91aa7b909e501da94fc9566
Rosa Perez <rosaperez@Rosas-MBP.attlocal.net>
2019-06-04 12:06:12 -0700

    adding Stripe

--- stripe.js
+++ stripe.js
@@ -2,11 +2,32 @@
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 const stripe = require('stripe')('sk_test_ZhPRczzPc38M67taa8x14w2N00IesVU1LX');
 
+
 (async () => {
-  const product = await stripe.products.create({
-    name: 'My SaaS Platform',
-    type: 'service',
+  const plan = await stripe.plans.create({
+    product: 'prod_FC5qFJ57wYzfW6',
+    nickname: 'SaaS Quizzer Platform USD',
+    currency: 'usd',
+    interval: 'month',
+    amount: 10000,
   });
 })();
 
 
+(async () => {
+  const customer = await stripe.customers.create({
+    email: 'jenny.rosen@example.com',
+    source: 'src_18eYalAHEMiOZZp1l9ZTjSU0',
+  });
+})();
+
+
+
+
+// {
+//   "id": "prod_F50D4B1SnFhG0D",
+//   "object": "product",
+
+//   "name": "Quizzer",
+//   "type": "service",
+// }
