cad4ad485cffee7b6afa3d60770a2ce3ef8c2c02
Marc Thiongo <marcthuo@gmail.com>
2019-06-11 18:57:09 -0400

    secondary push still in progress

--- data/seeds/009-quiz_student.js
+++ data/seeds/009-quiz_student.js
@@ -2,8 +2,6 @@ exports.seed = function(knex, Promise) {
   // Inserts seed entries
   return knex("quiz_student").insert([
     {
-      student_id: 6,
-      quiz_id: 1,
       score: 10,
     }
   ]);
--- data/seeds/010-student_answer.js
+++ data/seeds/010-student_answer.js
@@ -2,11 +2,7 @@ exports.seed = function(knex, Promise) {
   // Inserts seed entries
   return knex("student_answers").insert([
     {
-      student_id: 6,
       student_answer: 'B',
-      question_id: 1,
-      quiz_id: 1,
-      correct_answer: 'B',
     }
   ]);
 };
