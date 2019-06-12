deb9c04c1b9f772243112cca1db72a98ec0f672f
josephriosio <blankworldwide@gmail.com>
2019-06-12 14:54:57 -0700

    login changes

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -2,19 +2,24 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const db = require("../model/UserModel");
+const Teacherdb = require("../model/TeacherModel");
+const Studentdb = require("../model/StudentModel");
 const { authenticate } = require("../auth/authenticate");
 
-router.post("/login", authenticate, async (req, res) => {
+//teacher login
+router.post("/teacher/login", authenticate, async (req, res) => {
   try {
     const { email, name } = req.body.profileObj;
+    const role = "Teacher";
 
     const user = {
       email,
-      name
+      name,
+      role
     };
 
     console.log(user);
-    const ifUserExist = await db.findBy(user);
+    const ifUserExist = await Teacherdb.findBy(user);
     console.log(ifUserExist);
 
     if (ifUserExist.length > 0) {
@@ -22,7 +27,37 @@ router.post("/login", authenticate, async (req, res) => {
       return res.status(200).json(ifUserExist);
     }
 
-    const addUser = await db.add(user);
+    const addUser = await Teacherdb.add(user);
+
+    return res.status(201).json(addUser);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//student login
+router.post("/student/login", authenticate, async (req, res) => {
+  try {
+    const { email, name } = req.body.profileObj;
+    const role = "Student";
+
+    const user = {
+      email,
+      name,
+      role
+    };
+
+    console.log(user);
+    const ifUserExist = await Studentdb.findBy(user);
+    console.log(ifUserExist);
+
+    if (ifUserExist.length > 0) {
+      console.log("user exists");
+      return res.status(200).json(ifUserExist);
+    }
+
+    const addUser = await Studentdb.add(user);
 
     return res.status(201).json(addUser);
   } catch (err) {
