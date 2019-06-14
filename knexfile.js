895f49c08c54b68ed0ee624aa51981d0defefc1b
josephriosio <blankworldwide@gmail.com>
2019-06-14 13:23:27 -0700

    boom

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,44 @@
 // Update with your config settings.
-require('dotenv').config()
-const pg = require('pg')
-pg.defaults.ssl = true
+require("dotenv").config();
+const pg = require("pg");
+pg.defaults.ssl = true;
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './data/quizzer.db3'
+      filename: "./data/quizzer.db3"
     },
     seeds: {
-      directory: './data/seeds/'
+      directory: "./data/seeds/"
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     }
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/testDB.db3',
+      filename: "./data/testDB.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: "sqlite3",
+    connection: "./data/quizzer.db3",
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     },
     useNullAsDefault: true
   }
-}
+};
