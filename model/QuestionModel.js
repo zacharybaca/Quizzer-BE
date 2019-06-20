df3eb567442d54f6f94999f012df2247cfee2235
josephriosio <blankworldwide@gmail.com>
2019-06-20 16:27:38 -0700

    added curlys

--- model/QuestionModel.js
+++ model/QuestionModel.js
@@ -1,42 +1,42 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 module.exports = {
-    find,
-    findBy,
-    add,
-    findById,
-    remove,
-    update
+  find,
+  findBy,
+  add,
+  findById,
+  remove,
+  update
 };
 
 function find() {
-    return db('questions');
+  return db("questions");
 }
 
 function findBy(filter) {
-    return db('questions').where(filter);
+  return db("questions").where(filter);
 }
 
 async function add(data) {
-    const [id] = await db('questions').insert(data);
+  const { id } = await db("questions").insert(data);
 
-    return findById(id);
+  return findById(id);
 }
 
 function findById(id) {
-    return db('questions')
-    .where({id})
+  return db("questions")
+    .where({ id })
     .first();
 }
 
 function update(id, changes) {
-    return db('questions')
+  return db("questions")
     .where({ id })
     .update(changes);
 }
 
 function remove(id) {
-    return db('questions')
-    .where('id', id)
+  return db("questions")
+    .where("id", id)
     .del();
 }
